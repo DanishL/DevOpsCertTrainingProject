14fb8de4d537cc93fae715ea436d842d75f18fee
DanishL <59801180+DanishL@users.noreply.github.com>
2021-08-05 23:11:58 -0400

    Adding java file that will print a message

---
+++ DevOpsCertTrainingAssignment3.java
@@ -0,0 +1,6 @@
+
+public class DevOpsCertTrainingAssignment3{
+	public static void main(String[] str){
+		System.out.println("This is Dev Ops Certification Training assignment no 3 submitted by Danish Lodhi");
+	}
+}
